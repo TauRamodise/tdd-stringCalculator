d48449efca1f94372aedc64e9a84dba4ff3b3381
Dimpho Ramodise <dimpho.ramodise@gmail.com>
2019-06-14 11:37:22 +0200

    String calculator complete with steps 1 - 5

---
+++ spec/stringCalculatorSpec.js
@@ -0,0 +1,32 @@
+describe('Calculator', function () {
+    it('should take and empty string and return 0', function () {
+        let calculate = add(' ')
+        expect(calculate).toBe(0)
+    })
+    it('should take a string with numbers and add it up', function () {
+        let calculate = add('1, 2')
+        expect(calculate).toBe(3)
+    })
+    it('should add multiple numbers in a string', function () {
+        let calculate = add('1, 2, 3, 4, 5')
+        expect(calculate).toBe(15)
+    })
+    it('should add multiple numbers in a string with a next line separator', function () {
+        let calculate = add('1\n2, 4')
+        expect(calculate).toBe(7)
+    })
+    it('should add multiple numbers in a string with next line separators', function () {
+        let calculate = add('1\n2, 3\n7')
+        expect(calculate).toBe(13)
+    })
+    it('should add throw error when adding negative numbers ', function () {
+        let calculate = add('-1, -2')
+        expect(calculate).toBe('error no negative numbers allowed -1, -2');
+
+    })
+
+    it('should accept an expression with a delimiter and add the string', function () {
+        let calculate = add('//;\n1;2');
+        expect(calculate).toBe(3);
+    })
+})
---
+++ src/stringCalculator.js
@@ -0,0 +1,31 @@
+/* CODE EXPLANATION
+replace - replaces delimiter characters in string with an empty string 
+split - splits each character in the string to its own string 
+parseInt - converts a string to a number
+
+*/
+
+
+function add(str) {
+  let result = 0;
+
+  let replace = str.replace(/^(\/\/.\n)/, '');
+  let split = replace.split(/[//;\n,]/);
+
+  if (str === " ") {
+
+    return 0
+    
+  }
+  for (let i = 0; i < split.length; i++) {
+
+    let sum = parseInt(split[i]);
+
+    if (sum < 0) {
+      return 'error no negative numbers allowed ' + '' + str;
+    }
+    result += sum;
+  }
+  return result;
+}
+
